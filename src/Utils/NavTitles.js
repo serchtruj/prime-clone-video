7ee145e5d718fba84139e7109e229d6af0e4f0a7
serchtruj <serchtruj1@gmail.com>
2023-06-27 00:25:34 -0500

    Add navbar necessary titles and item component

---
+++ src/Utils/NavTitles.js
@@ -0,0 +1,48 @@
+export const NavTitles = [
+    {
+        name: "Inicio",
+        icon: "icon",
+        subTitles : ["Todo", "Peliculas", "Series", "Deportes"]
+    },
+    {
+        name: "Tienda",
+        icon: "icon",
+        subTitles: ["Todo", "Alquilar o comprar","Canales"]
+    },
+    {
+        name: "TV en directo",
+        icon: "",
+        subTitles: []
+    },
+    {
+        name: "Categorias",
+        icon: "icon",
+        subTitles: [
+            {
+                name: "Generos",
+                options: [
+                    "Accion y aventuras", 
+                    "Anime", "Comedi", 
+                    "Documental", 
+                    "Drama",
+                    "Fantasía", 
+                    "Terror", 
+                    "Internacional", 
+                    "Ninos",
+                    "Videos musicales",
+                    "Romance", 
+                    "Misterio y suspense"
+                ]
+            },
+            {
+                name: "Colecciones destacadas",
+                options: ["Deportes", "LGBTQIA+", "Del cine a casa"]
+            }
+        ]
+    },
+    {
+        name: "Mi espacio",
+        icon: "icon",
+        subTitles: ["Todo", "Mi lista", "Titulos comprados y aquilados"]
+    }
+]
--- src/pages/NavBar/NavBar.tsx
+++ src/pages/NavBar/NavBar.tsx
@@ -1,9 +1,22 @@
 import './NavBar.css';
+import { NavTitles } from '../../Utils/NavTitles';
 
-function NavBar() {   
+function NavBar() {
+    function NavBarItem(props: {text: string, icon: string}) {
+        return (
+            <div className="NavBarItem">
+                <h3>{props.text}</h3>
+                <h3>{props.icon}</h3>
+            </div>
+        );
+    }
+    console.log(NavTitles)
     return (
         <div className="NavBar">
-            <h1>NavBar</h1>
+            <h2>Prime Video Clone</h2>
+            <p>Search Button</p>
+            <p>Profile Name</p>
+            <p>Avatar image</p>
         </div>
     );
 } 
